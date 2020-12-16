4c7e07f32d99d86965c1a7343445de20fb3526e5
Henrietta Holze <szr214@alumni.ku.dk>
2020-12-16 21:17:05 +0100

    day 12, part 2

--- 12.py
+++ 12.py
@@ -56,3 +56,54 @@ assert(follow_instructions(INPUT) == 25)
 with open('12.txt', 'r') as o:
     lines = o.read().splitlines()
     print(follow_instructions(lines))
+
+
+def equal_out(waypoint):
+    if waypoint['S'] > waypoint['N']:
+        waypoint['S'] -= waypoint['N']
+        waypoint['N'] = 0
+    elif waypoint['N'] > waypoint['S']:
+        waypoint['N'] -= waypoint['S']
+        waypoint['S'] = 0
+    if waypoint['E'] > waypoint['W']:
+        waypoint['E'] -= waypoint['W']
+        waypoint['W'] = 0
+    elif waypoint['W'] > waypoint['E']:
+        waypoint['W'] -= waypoint['E']
+        waypoint['E'] = 0
+
+    return waypoint
+
+
+def waypoint_instructions(INPUT):
+    # The waypoint starts 10 units east and 1 unit north relative to the ship
+    waypoint = {'E': 10, 'N': 1, 'W': 0, 'S': 0}
+
+    movements = {'E': 0, 'N': 0, 'W': 0, 'S': 0}
+
+    for command in INPUT:
+        if command[0] == 'F':
+            for direction, steps in waypoint.items():
+                movements[direction] += steps * int(command[1:])
+        elif command[0] == 'L':
+            for i in range(int(int(command[1:]) / 90)):
+                waypoint['N'], waypoint['W'], waypoint['S'], waypoint['E'] = waypoint['E'], waypoint['N'], waypoint['W'], waypoint['S']
+        elif command[0] == 'R':
+            for i in range(int(int(command[1:]) / 90)):
+                # E 10, N 4 -> S 10, E 4
+                waypoint['W'], waypoint['S'], waypoint['E'], waypoint['N'] = waypoint['S'], waypoint['E'], waypoint['N'], waypoint['W']
+        else:
+            waypoint[command[0]] += int(command[1:])
+            
+        # not really necessary
+        # waypoint = equal_out(waypoint)
+        # movements = equal_out(movements)
+        
+        # print(waypoint, movements)
+
+    manhattan_distance = abs(movements['E'] - movements['W']) + abs(movements['S'] - movements['N'])
+    return manhattan_distance
+
+assert(waypoint_instructions(INPUT) == 286)
+
+print(waypoint_instructions(lines))
